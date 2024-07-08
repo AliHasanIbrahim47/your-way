060de84d952270041cadf30623c56dfbc0e390a1
AliHasanIbrahim47 <ew9361903@gmail.com>
2024-07-08 20:47:00 +0300

    commit

--- src/pages/ShowUser/ShowUser.jsx
+++ src/pages/ShowUser/ShowUser.jsx
@@ -114,21 +114,22 @@ const ShowUser = () => {
           <div className="head-driver">
             <h1>{t("users.driver")}</h1>
             <div className="filter">
-              <label htmlFor="userType">{t("users.filter")}: </label>
+              <label htmlFor="userType">{t("travels.filter")} </label>
               <select id="userType" value={selectedType} onChange={handleTypeChange}>
-                <option value="future">{t("users.all")}</option>
-                <option value="old">{t("users.user")}</option>
+                <option value="future">{t("travels.future")}</option>
+                <option value="old">{t("travels.old")}</option>
               </select>
             </div>
           </div>
           <div className="user-details">
             <p><strong>{t("showuser.name")}:</strong> {user.full_name}</p>
             <p><strong>{t("showuser.phone")}:</strong> {user.phone}</p>
-            {userData.brand && <p><strong>{t("addusers.brand")}:</strong> {userData.brand}</p>}
-            {userData.model && <p><strong>{t("addusers.model")}:</strong> {userData.model}</p>}
-            {userData.capacity && <p><strong>{t("addusers.capacity")}:</strong> {userData.capacity}</p>}
+            {/* {userData.travel.bus.brand && <p><strong>{t("addusers.brand")}:</strong> {userData.travel.bus.brand}</p>}
+            {userData.travel.bus.model && <p><strong>{t("addusers.model")}:</strong> {userData.travel.bus.model}</p>}
+            {userData.travel.bus.capacity && <p><strong>{t("addusers.capacity")}:</strong> {userData.travel.bus.capacity}</p>} */}
           </div>
           <div className="table-container">
+            <h2>{t("travels.publicTravels")}</h2>
             <table>
               <thead>
                 <tr>
@@ -143,18 +144,51 @@ const ShowUser = () => {
               </thead>
               <tbody>
                 {userData.map((element, index) => (
-                  <tr key={index}>
-                    <td><Moment format="YYYY/MM/DD">{element.travel.starting_date}</Moment></td>
-                    <td>{element.travel.going_time}</td>
-                    <td>{element.travel.going_from}</td>
-                    <td><Moment format="YYYY/MM/DD">{element.travel.ending}</Moment></td>
-                    <td>{element.travel.returning_time}</td>
-                    <td>{element.travel.returning_from}</td>
-                    <td>{element.travel.price}</td>
-                  </tr>
+                  element.type === "public" ? (
+                    <tr key={index}>
+                      <td><Moment format="YYYY/MM/DD">{element.travel.starting_date}</Moment></td>
+                      <td>{element.travel.going_time}</td>
+                      <td>{element.travel.going_from}</td>
+                      <td><Moment format="YYYY/MM/DD">{element.travel.ending}</Moment></td>                      
+                      <td>{element.travel.returning_time}</td>
+                      <td>{element.travel.returning_from}</td>
+                      <td>{element.travel.price}</td>
+                    </tr>
+                  ) : (
+                    null
+                  )
                 ))}
               </tbody>
             </table>
+          </div>
+          <div className="private-table">
+          <div className="table-container">
+            <h2>{t("travels.privateTravels")}</h2>
+            <table>
+              <thead>
+                <tr>
+                  <th>{t("travels.sDate")}</th>
+                  <th>{t("travels.going_from")}</th>
+                  <th>{t("travels.going_to")}</th>
+                  <th>{t("travels.status")}</th>
+                </tr>
+              </thead>
+              <tbody>
+                {userData.map((element, index) => (
+                  element.type === "private" ? (
+                    <tr key={index}>
+                      <td><Moment format="YYYY/MM/DD">{element.going_date}</Moment></td> 
+                      <td>{element.going_from}</td>
+                      <td>{element.going_to}</td>
+                      <td>{element.status}</td>
+                    </tr>
+                  ) : (
+                    null
+                  )
+                ))}
+              </tbody>
+            </table>
+          </div>
           </div>
         </div>
       </div>
