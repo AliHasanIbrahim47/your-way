4a632c4022340fc2113423adeb6639729e537874
AliHasanIbrahim47 <ew9361903@gmail.com>
2024-07-20 10:26:06 -0500

    m20

--- src/pages/ShowUser/ShowUser.jsx
+++ src/pages/ShowUser/ShowUser.jsx
@@ -6,6 +6,8 @@ import axios from 'axios';
 import Spinner from "../../components/Spinner";
 import { useTranslation } from 'react-i18next';
 import Moment from 'react-moment';
+import Select from 'react-select';
+import Popup from '../../components/Popup';
 
 const ShowUser = () => {
   const [t, i18n] = useTranslation("global");
@@ -13,6 +15,10 @@ const ShowUser = () => {
   const [user, setUser] = useState(null);
   const [userData, setUserData] = useState([]);
   const [selectedType, setSelectedType] = useState('future');
+  const [showPopup, setShowPopup] = useState(false);
+  const [payments, setPayments] = useState([]);
+  const [newPaymentAmount, setNewPaymentAmount] = useState(0);
+  const [paymentUpdates, setPaymentUpdates] = useState({});
 
   const token = localStorage.getItem('token');
 
@@ -61,12 +67,184 @@ const ShowUser = () => {
     fetchUsersData();
   }, [selectedType, token, id]);
 
-  useEffect(() => {
-    console.log('Updated userData:', userData);
-  }, [userData]);
-
   const handleTypeChange = (event) => {
     setSelectedType(event.target.value);
+  };
+
+  // holidays and reservations 
+
+  const [reservationDays, setReservationDays] = useState([]);
+  const [holidays, setHolidays] = useState([]);
+  const [newHoliday, setNewHoliday] = useState("");
+  const [selectedHolidays, setSelectedHolidays] = useState([]);
+  const daysOptions = [
+    { value: 'Monday', label: t("week.Monday") },
+    { value: 'Tuesday', label: t("week.Tuesday") },
+    { value: 'Wednesday', label: t("week.Wednesday") },
+    { value: 'Thursday', label: t("week.Thursday") },
+    { value: 'Friday', label: t("week.Friday") },
+    { value: 'Saturday', label: t("week.Saturday") },
+    { value: 'Sunday', label: t("week.Sunday") }
+  ];
+
+  const [selectedDays, setSelectedDays] = useState([]);
+
+  useEffect(() => {
+    fetchReservationDays();
+    fetchHolidays();
+    fetchPayments();
+  }, []);
+
+  const fetchReservationDays = async () => {
+    try {
+      const response = await axios.get(`${baseURL}/days`, 
+        {
+          headers: {
+            'Authorization': `Bearer ${token}`,
+          },
+        }
+      );
+      setReservationDays(response.data.data);
+    } catch (error) {
+      console.error("Error fetching reservation days", error);
+    }
+  };
+
+  const fetchHolidays = async () => {
+    try {
+      const response = await axios.get(`${baseURL}/holidays/${id}`, 
+        {
+          headers: {
+            'Authorization': `Bearer ${token}`,
+          },
+        }
+      );
+      setHolidays(response.data.data);
+      console.log(holidays);
+    } catch (error) {
+      console.error("Error fetching holidays", error);
+    }
+  };
+
+  const fetchPayments = async () => {
+    try {
+      const response = await axios.get(`${baseURL}/commissions/driver/${id}`, {
+        headers: {
+          'Authorization': `Bearer ${token}`,
+        },
+      });
+      setPayments(response.data.data);
+    } catch (error) {
+      console.error("Error fetching payments", error);
+    }
+  };
+
+  const addReservationDays = async () => {
+    const newDays = selectedDays.map(day => day.value);
+    const allDays = [...new Set([...reservationDays, ...newDays])];
+    try {
+      await axios.post(`${baseURL}/days`, { days: allDays }, {
+        headers: {
+          'Authorization': `Bearer ${token}`,
+        }
+      });
+      fetchReservationDays();
+      setSelectedDays([]);
+    } catch (error) {
+      console.error("Error adding reservation days", error);
+    }
+  };
+
+  const addHolidays = async () => {
+    try {
+      const existingHolidays = holidays.map(h => h.date.split('T')[0]);
+      const newHolidayString = newHoliday;
+      const allHolidaysSet = new Set([...existingHolidays, newHolidayString]);
+      const allHolidays = Array.from(allHolidaysSet); 
+      await axios.post(`${baseURL}/holidays/${id}`, { dates: allHolidays }, {
+        headers: {
+          'Authorization': `Bearer ${token}`,
+        }
+      });
+      fetchHolidays(); 
+      setNewHoliday(""); 
+    } catch (error) {
+      console.error("Error adding holidays", error);
+    }
+  };
+
+  const toggleHolidaySelection = (holidayId) => {
+    setSelectedHolidays((prevSelected) => {
+      if (prevSelected.includes(holidayId)) {
+        return prevSelected.filter(id => id !== holidayId);
+      } else {
+        return [...prevSelected, holidayId];
+      }
+    });
+  };
+
+  const confirmDeleteHolidays = () => {
+    setShowPopup(true);
+  };
+
+  const deleteHolidays = async () => {
+    try {
+      await axios.delete(`${baseURL}/holidays/${id}`, { ids: selectedHolidays }, {
+        headers: {
+          'Authorization': `Bearer ${token}`,
+        }
+      });
+      fetchHolidays();
+      setSelectedHolidays([]);
+    } catch (error) {
+      console.error("Error deleting holidays", error);
+    } finally {
+      setShowPopup(false);
+    }
+  };
+
+  const handleAddPayment = async () => {
+    try {
+      await axios.post(`${baseURL}/commissions`, 
+      { 
+        driver_id: id, 
+        amount: newPaymentAmount,
+        currency: "USD" // or whatever currency you need
+      }, 
+      {
+        headers: {
+          'Authorization': `Bearer ${token}`,
+        }
+      });
+      fetchPayments(); 
+      setNewPaymentAmount(0); 
+    } catch (error) {
+      console.error("Error adding payment", error);
+    }
+  };
+
+  const handleUpdatePayment = async (paymentId, newAmount) => {
+    try {
+      await axios.put(`${baseURL}/commissions/${paymentId}`, 
+      { 
+        amount: newAmount
+      }, 
+      {
+        headers: {
+          'Authorization': `Bearer ${token}`,
+        }
+      });
+      fetchPayments(); 
+    } catch (error) {
+      console.error("Error updating payment", error);
+    }
+  };
+
+  const handlePaymentChange = (paymentId, newAmount) => {
+    setPaymentUpdates(prev => ({
+      ...prev,
+      [paymentId]: newAmount
+    }));
   };
 
   if (loader) {
@@ -111,8 +289,7 @@ const ShowUser = () => {
       <div className="showuser">
         <Sidebar />
         <div className="container">
-          <div className="head-driver">
-            <h1>{t("users.driver")}</h1>
+          <div className="table-type">
             <div className="filter">
               <label htmlFor="userType">{t("travels.filter")} </label>
               <select id="userType" value={selectedType} onChange={handleTypeChange}>
@@ -121,78 +298,155 @@ const ShowUser = () => {
               </select>
             </div>
           </div>
+
           <div className="user-details">
-            <p><strong>{t("showuser.name")}:</strong> {user.full_name}</p>
-            <p><strong>{t("showuser.phone")}:</strong> {user.phone}</p>
-            {/* {userData.travel.bus.brand && <p><strong>{t("addusers.brand")}:</strong> {userData.travel.bus.brand}</p>}
-            {userData.travel.bus.model && <p><strong>{t("addusers.model")}:</strong> {userData.travel.bus.model}</p>}
-            {userData.travel.bus.capacity && <p><strong>{t("addusers.capacity")}:</strong> {userData.travel.bus.capacity}</p>} */}
-          </div>
-          <div className="private-table">
-          <div className="table-container">
-            <h2>{t("travels.publicTravels")}</h2>
-            <table>
-              <thead>
-                <tr>
-                  <th>{t("travels.sDate")}</th>
-                  <th>{t("travels.sTime")}</th>
-                  <th>{t("travels.going_from")}</th>
-                  <th>{t("travels.aDate")}</th>
-                  <th>{t("travels.aTime")}</th>
-                  <th>{t("travels.going_to")}</th>
-                  <th>{t("extra.price")}</th>
-                </tr>
-              </thead>
-              <tbody>
-                {userData.map((element, index) => (
-                  element.type === "public" ? (
-                    <tr key={index}>
-                      <td><Moment format="YYYY/MM/DD">{element.travel.starting_date}</Moment></td>
-                      <td>{element.travel.going_time}</td>
-                      <td>{element.travel.going_from}</td>
-                      <td><Moment format="YYYY/MM/DD">{element.travel.ending}</Moment></td>                      
-                      <td>{element.travel.returning_time}</td>
-                      <td>{element.travel.returning_from}</td>
-                      <td>{element.travel.price}</td>
-                    </tr>
-                  ) : (
-                    null
-                  )
+            <div className="details">
+              <p><strong>{t("usersOnly.name")}:</strong> {user.full_name}</p>
+              <p><strong>{t("usersOnly.phone")}:</strong> {user.phone}</p>
+            </div>
+            
+            {/* Reservation Days */}
+            <div className="reservation-days">
+              <h2>{t("holidays.resDays")}</h2>
+              <ul>
+                {reservationDays.map((day, index) => (
+                  <li key={index}>{t(`week.${day}`)}</li>
                 ))}
-              </tbody>
-            </table>
-          </div>
-          </div>
-          <div className="private-table">
-          <div className="table-container">
-            <h2>{t("travels.privateTravels")}</h2>
-            <table>
-              <thead>
-                <tr>
-                  <th>{t("travels.sDate")}</th>
-                  <th>{t("travels.going_from")}</th>
-                  <th>{t("travels.going_to")}</th>
-                  <th>{t("travels.status")}</th>
-                </tr>
-              </thead>
-              <tbody>
-                {userData.map((element, index) => (
-                  element.type === "private" ? (
-                    <tr key={index}>
-                      <td><Moment format="YYYY/MM/DD">{element.going_date}</Moment></td> 
-                      <td>{element.going_from}</td>
-                      <td>{element.going_to}</td>
-                      <td>{element.status}</td>
-                    </tr>
-                  ) : (
-                    null
-                  )
+              </ul>
+              <Select
+                isMulti
+                value={selectedDays}
+                options={daysOptions}
+                onChange={setSelectedDays}
+                placeholder={t("holidays.selectDays")}
+              />
+              <button onClick={addReservationDays}>{t("holidays.addDays")}</button>
+            </div>
+
+            {/* Display Holidays */}
+            <div className="holidays">
+              <h2>{t("holidays.title")}</h2>
+              <ul>
+                {holidays.map((holiday) => (
+                  <li key={holiday.id}>
+                    <input
+                      type="checkbox"
+                      checked={selectedHolidays.includes(holiday.id)}
+                      onChange={() => toggleHolidaySelection(holiday.id)}
+                    />
+                    <Moment format="YYYY/MM/DD">{holiday.date}</Moment>
+                  </li>
                 ))}
-              </tbody>
-            </table>
+              </ul>
+              <input
+                type="date"
+                value={newHoliday}
+                onChange={(e) => setNewHoliday(e.target.value)}
+              />
+              <button onClick={addHolidays}>{t("holidays.addHoliday")}</button>
+              <button onClick={confirmDeleteHolidays}>{t("holidays.deleteSelected")}</button>
+            </div>
+
+            {/* Display Payments */}
+            <div className="payment">
+              <h2>{t("holidays.title1")}</h2>
+              <ul>
+                {payments.map((payment) => (
+                  <li key={payment.id}>
+                    <span>{payment.amount} {payment.currency}</span>
+                    <input
+                      type="number"
+                      value={paymentUpdates[payment.id] || payment.amount}
+                      onChange={(e) => handlePaymentChange(payment.id, e.target.value)}
+                    />
+                    <button onClick={() => handleUpdatePayment(payment.id, paymentUpdates[payment.id] || payment.amount)}>
+                      {t("holidays.update")}
+                    </button>
+                  </li>
+                ))}
+              </ul>
+              <div className='edit-payment'>
+                <input
+                  type="number"
+                  value={newPaymentAmount}
+                  onChange={(e) => setNewPaymentAmount(e.target.value)}
+                />
+                <button onClick={handleAddPayment}>{t("holidays.addPayment")}</button>
+              </div>
+            </div>
           </div>
+
+          {/* Public Travels */}
+          <div className="private-table">
+            <div className="table-container">
+              <h2>{t("travels.publicTravels")}</h2>
+              <table>
+                <thead>
+                  <tr>
+                    <th>{t("travels.sDate")}</th>
+                    <th>{t("travels.sTime")}</th>
+                    <th>{t("travels.going_from")}</th>
+                    <th>{t("travels.aDate")}</th>
+                    <th>{t("travels.aTime")}</th>
+                    <th>{t("travels.going_to")}</th>
+                    <th>{t("extra.price")}</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {userData.map((element, index) =>
+                    element.type === "public" ? (
+                      <tr key={index}>
+                        <td><Moment format="YYYY/MM/DD">{element.travel.starting_date}</Moment></td>
+                        <td>{element.travel.going_time}</td>
+                        <td>{element.travel.going_from}</td>
+                        <td><Moment format="YYYY/MM/DD">{element.travel.ending}</Moment></td>
+                        <td>{element.travel.returning_time}</td>
+                        <td>{element.travel.returning_from}</td>
+                        <td>{element.travel.price}</td>
+                      </tr>
+                    ) : null
+                  )}
+                </tbody>
+              </table>
+            </div>
+          </div>
+
+          {/* Private Travels */}
+          <div className="private-table">
+            <div className="table-container">
+              <h2>{t("travels.privateTravels")}</h2>
+              <table>
+                <thead>
+                  <tr>
+                    <th>{t("travels.sDate")}</th>
+                    <th>{t("travels.going_from")}</th>
+                    <th>{t("travels.going_to")}</th>
+                    <th>{t("travels.status")}</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {userData.map((element, index) =>
+                    element.type === "private" ? (
+                      <tr key={index}>
+                        <td><Moment format="YYYY/MM/DD">{element.going_date}</Moment></td>
+                        <td>{element.going_from}</td>
+                        <td>{element.going_to}</td>
+                        <td>{element.status}</td>
+                      </tr>
+                    ) : null
+                  )}
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
+        {showPopup && (
+          <Popup
+            message={t("holidays.confirmDelete")}
+            onConfirm={deleteHolidays}
+            onCancel={() => setShowPopup(false)}
+          />
+        )}
       </div>
     );
   }
